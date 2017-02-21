81a3fda3a221172709631d7229f7c7861d4979ca
landonrs <altruist8@yahoo.com>
2017-02-20 19:33:03 -0700

    Initial commit

---
+++ android/src/com/cs246team01/bugtag/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cs246team01.bugtag;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cs246team01.bugtag.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
