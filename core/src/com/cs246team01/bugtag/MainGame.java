0897d7f320d056207e6d8815703a9907b97bfe3f
Likhith Chitneni <git@chittti.com>
2017-02-25 21:37:15 -0700

    Removed red background

--- core/src/com/cs246team01/bugtag/MainGame.java
+++ core/src/com/cs246team01/bugtag/MainGame.java
@@ -38,7 +38,7 @@ public class MainGame extends ApplicationAdapter implements InputProcessor{
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
         //this is where we run our game
