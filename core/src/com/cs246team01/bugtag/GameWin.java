27d96e88f49a545152a55cea82554b2895361f16
Cameron Lewis <cameronwlewis@gmail.com>
2017-03-16 19:17:58 -0600

    made JavaDoc for GameWin class

--- core/src/com/cs246team01/bugtag/GameWin.java
+++ core/src/com/cs246team01/bugtag/GameWin.java
@@ -4,15 +4,24 @@ import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created by cameronlewis on 3/16/17.
+ * Responsible for tracking and checking if the game should end, based on whether the bugs are touching.
  */
-
 class GameWin {
     private int win_status;
 
+    /**
+     * Default constructor which sets initial value of the win_status and nothing else.
+     */
     GameWin(){
         win_status = 0;
     }
 
+    /**
+     *
+     * @param bugGame Needed parameter to obtain the hit boxes of each bug and their current positions.
+     * @return Returns '3' to represent GAME_OVER if bugs are touching, or '1' to represent
+     * GAME_STARTED (meaning the current game should still continue) if they're not touching.
+     */
     int checkWin(GridObjectHandler bugGame){
         Rectangle bug1_hitbox = bugGame.getBugOne().getHitBox();
         Rectangle bug2_hitbox = bugGame.getBugTwo().getHitBox();
@@ -26,10 +35,17 @@ class GameWin {
              return win_status = 1;
      }
 
+    /**
+     * Resets the status that reports whether the game has been won.
+     */
      private void resetWinStatus(){
          win_status = 0;
      }
 
+    /**
+     * Determines whether the game should be reset based on the win_status.
+     * @return Returns true if game reset is needed, and false if not.
+     */
      boolean isResetNeeded(){
          if (win_status == 3) {
              resetWinStatus();
